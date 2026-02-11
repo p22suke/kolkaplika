37d5ad270a18ad881288ecefec7f3986f8069e83
siina.P22suke <liina@kolkaplika.ee>
2026-02-11 15:23:25 +0200

    m6tlesin l2bi, ikka palju now, now, now, now, now, now. esile tuleb ka get, display

--- Time/Machine/Now.java
+++ Time/Machine/Now.java
@@ -3,23 +3,22 @@ import java.time.Instant;
 
 public class Now {
     private final Instant now; //MUUTUMATU MUUTUSTES
-    //siin nimetame sinise now, variable
 
     public Now() {
         this.now = Instant.now(); //HETKE LOOMINE
-    } //meetod
+    }
 
     public Instant getNow() {
-        return now;
-    }// meetod 
+        return now; //HETKE ARVUTAMINE
+    }
 
     public void displayNow() {
-        System.out.println("Instant now: " + now);
+        System.out.println("Instant now: " + now);//HETK INFONA EKRAANIL
     }
 
     public static void main(String[] args) {
-       Now now = new Now();
-       now.displayNow(); 
+       Now now = new Now(); // klass variable = function run the method to create the moment
+       now.displayNow(); // variable.displayresults
     }
 }
 
@@ -31,4 +30,6 @@ i ran and got
 Instant now: 2026-02-11T13:13:45.133952Z
 
 Instant Now m6istmine eeldab mult gregooriuse kalendri m6istmist.. millegip2rast lootsin siin sekundeid n2ha?
+
+probleemne on ka MASIN v ENGINE v MOOTOR. juba m6tlesin ka kutsuda AJUx. time brain. kolkaplika time brain. pls, yes. LOL
  */
